7e29b1091c1075474aa56798f307b9bea31572e6
Dilshad <a-rustacean@outlook.com>
2026-05-27 18:13:16 +0530

    fix(features/hyprland): preserve `hyprland.passthru.providedSessions`

--- modules/features/hyprland.nix
+++ modules/features/hyprland.nix
@@ -21,35 +21,38 @@
       ...
     }:
     {
-      packages.hyprland = inputs.wrapper-modules.lib.wrapPackage (
-        { ... }:
-        {
-          inherit pkgs;
-          package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-          runtimePkgs = with pkgs; [
-            inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
-            self'.packages.hypridle
-            self'.packages.hyprsunset
-            hyprpicker
-            cliphist
-            wl-clipboard
-            # TODO: dunst
-            # TODO: quickshell
-            # TODO: gtk?
-            # TODO: cursor
-            # TODO: dunst
-            # TODO: hyprshutdown
-          ];
-          flags = {
-            "--config" = "${../../hyprland}/hyprland.lua";
-          };
-          env = {
-            HYPRLAND_ALACRITTY_EXE = lib.getExe self'.packages.alacritty;
-            HYPRLAND_ROFI_EXE = lib.getExe pkgs.rofi; # TODO: configure rofi
-            HYPRLAND_HYPRPAPER_EXE = lib.getExe self'.packages.hyprpaper;
-            HYPRLAND_QUICKSHELL_EXE = lib.getExe self'.packages.quickshell;
-          };
-        }
-      );
+      packages.hyprland =
+        lib.extendDerivation true inputs.hyprland.packages.aarch64-linux.hyprland.passthru
+          (
+            inputs.wrapper-modules.lib.wrapPackage (
+              { ... }:
+              {
+                inherit pkgs;
+                package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+                runtimePkgs = with pkgs; [
+                  inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
+                  self'.packages.hypridle
+                  self'.packages.hyprsunset
+                  hyprpicker
+                  cliphist
+                  wl-clipboard
+                  # TODO: dunst
+                  # TODO: gtk?
+                  # TODO: cursor
+                  # TODO: dunst
+                  # TODO: hyprshutdown
+                ];
+                flags = {
+                  "--config" = "${../../hyprland}/hyprland.lua";
+                };
+                env = {
+                  HYPRLAND_ALACRITTY_EXE = lib.getExe self'.packages.alacritty;
+                  HYPRLAND_ROFI_EXE = lib.getExe pkgs.rofi; # TODO: configure rofi
+                  HYPRLAND_HYPRPAPER_EXE = lib.getExe self'.packages.hyprpaper;
+                  HYPRLAND_QUICKSHELL_EXE = lib.getExe self'.packages.quickshell;
+                };
+              }
+            )
+          );
     };
 }
