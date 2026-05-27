f74b73a2bb03dea0cac327e750e26cbdfcc110ca
Dilshad <a-rustacean@outlook.com>
2026-05-27 12:31:55 +0530

    flake: de-dup deps

--- flake.nix
+++ flake.nix
@@ -1,35 +1,64 @@
 {
+  description = "Bleeding Edge NixOS Setup";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
-    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    wrapper-modules = {
+      url = "github:BirdeeHub/nix-wrapper-modules";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     hyprland = {
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # honestly, this feels wrong, maybe a better way to use all hypr* pkgs
     hypridle = {
       url = "github:hyprwm/hypridle";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "hyprland/systems";
+      inputs.hyprland-protocols.follows = "hyprland/hyprland-protocols";
+      inputs.hyprlang.follows = "hyprland/hyprlang";
+      inputs.hyprutils.follows = "hyprland/hyprutils";
+      inputs.hyprwayland-scanner.follows = "hyprland/hyprwayland-scanner";
     };
 
     hyprpaper = {
       url = "github:hyprwm/hyprpaper";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "hyprland/systems";
+      inputs.aquamarine.follows = "hyprland/aquamarine";
+      inputs.hyprgraphics.follows = "hyprland/hyprgraphics";
+      inputs.hyprtoolkit.follows = "hyprland/hyprland-guiutils/hyprtoolkit";
+      inputs.hyprlang.follows = "hyprland/hyprlang";
+      inputs.hyprwire.follows = "hyprland/hyprwire";
+      inputs.hyprutils.follows = "hyprland/hyprutils";
+      inputs.hyprwayland-scanner.follows = "hyprland/hyprwayland-scanner";
     };
 
     hyprlock = {
       url = "github:hyprwm/hyprlock";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "hyprland/systems";
+      inputs.hyprgraphics.follows = "hyprland/hyprgraphics";
+      inputs.hyprlang.follows = "hyprland/hyprlang";
+      inputs.hyprutils.follows = "hyprland/hyprutils";
+      inputs.hyprwayland-scanner.follows = "hyprland/hyprwayland-scanner";
     };
 
     hyprsunset = {
       url = "github:hyprwm/hyprsunset";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "hyprland/systems";
+      inputs.hyprland-protocols.follows = "hyprland/hyprland-protocols";
+      inputs.hyprlang.follows = "hyprland/hyprlang";
+      inputs.hyprutils.follows = "hyprland/hyprutils";
+      inputs.hyprwayland-scanner.follows = "hyprland/hyprwayland-scanner";
     };
   };
 
