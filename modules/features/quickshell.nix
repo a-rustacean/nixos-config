a587fdaf3512a2b69646add913cd89e3c2e92e0c
Dilshad <a-rustacean@outlook.com>
2026-06-22 15:16:56 +0530

    fix(qs): add `jq` to rumtimePkgs

--- modules/features/quickshell.nix
+++ modules/features/quickshell.nix
@@ -5,6 +5,7 @@
     {
       packages.quickshell = inputs.wrapper-modules.wrappers.quickshell.wrap {
         inherit pkgs;
+        runtimePkgs = [ pkgs.jq ];
         configDir = "${../../quickshell}";
         env.XDG_DATA_DIRS = "${pkgs.nerd-fonts.jetbrains-mono}/share";
       };
