6d9beb138f2f08dffebd8b31d11f9afd3e29ff7c
Dilshad <a-rustacean@outlook.com>
2026-06-25 16:34:36 +0530

    feat(opencode): add LSPs

--- modules/features/opencode.nix
+++ modules/features/opencode.nix
@@ -5,6 +5,18 @@
     {
       packages.opencode = self.lib.wrappers.opencode.wrap {
         inherit pkgs;
+        runtimePkgs = with pkgs; [
+          typescript-language-server
+          vscode-langservers-extracted
+          bash-language-server
+          lua-language-server
+          nixd
+          just-lsp
+          taplo
+        ];
+        settings = {
+          lsp = true;
+        };
         tui = {
           theme = "catppuccin";
         };
