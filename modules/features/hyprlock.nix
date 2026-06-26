a0f7e4de5aa18af91a35580a30539c7d35e526e9
Dilshad <a-rustacean@outlook.com>
2026-06-26 15:05:40 +0530

    feat(hyprlock): use catppuccin theme + UI changes

--- modules/features/hyprlock.nix
+++ modules/features/hyprlock.nix
@@ -1,4 +1,8 @@
 { self, ... }:
+let
+  ctp = self.lib.generators.catppuccin.mocha;
+  hex = color: builtins.substring 1 6 color;
+in
 {
   perSystem =
     { pkgs, ... }:
@@ -8,7 +12,8 @@
         settings = {
           general = {
             hide_cursor = true;
-            ignore_empty_input = true;
+            ignore_empty_input = false;
+            grace = 300;
           };
 
           animations = {
@@ -26,24 +31,56 @@
           background = [
             {
               path = "${../../wallpaper.jpg}";
-              blur_passes = 3;
+              blur_passes = 4;
               blur_size = 8;
+              contrast = 0.8;
+              brightness = 0.5;
+            }
+          ];
+
+          label = [
+            {
+              text = "cmd[update:1000] date '+%I:%M %p'";
+              font_size = 96;
+              font_family = "JetBrainsMono Nerd Font";
+              color = "rgb(${hex ctp.text})";
+              position = "0, 120";
+              monitor = "";
+              halign = "center";
+              valign = "center";
+            }
+            {
+              text = "cmd[update:3600] date '+%A, %B %d'";
+              font_size = 24;
+              font_family = "JetBrainsMono Nerd Font";
+              color = "rgb(${hex ctp.subtext0})";
+              position = "0, 40";
+              monitor = "";
+              halign = "center";
+              valign = "center";
             }
           ];
 
           input-field = [
             {
-              size = "200, 50";
-              position = "0, -80";
+              size = "320, 54";
+              position = "0, -70";
               monitor = "";
               dots_center = true;
               fade_on_empty = false;
-              font_color = "rgb(202, 211, 245)";
-              inner_color = "rgb(91, 96, 120)";
-              outer_color = "rgba(24, 25, 38, 100)";
-              outline_thickness = 5;
               placeholder_text = "Password...";
+              outline_thickness = 2;
+              dots_size = 0.2;
+              dots_spacing = 0.3;
+              font_color = "rgb(${hex ctp.text})";
+              inner_color = "rgb(${hex ctp.surface0})";
+              outer_color = "rgb(${hex ctp.lavender})";
+              fail_color = "rgb(${hex ctp.red})";
+              fail_text = "<i>Incorrect...</i>";
+              capslock_color = "rgb(${hex ctp.yellow})";
+              check_color = "rgb(${hex ctp.green})";
               shadow_passes = 2;
+              rounding = 12;
             }
           ];
         };
