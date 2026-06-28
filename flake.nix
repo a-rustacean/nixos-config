675a874a9f499af95c7943e77296e92704e2d6f0
Dilshad <a-rustacean@outlook.com>
2026-06-28 10:40:20 +0530

    feat: add zen-browser and catppuccin-zen-browser flake inputs

--- flake.nix
+++ flake.nix
@@ -15,6 +15,16 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    catppuccin-zen-browser = {
+      url = "github:catppuccin/zen-browser";
+      flake = false;
+    };
+
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
