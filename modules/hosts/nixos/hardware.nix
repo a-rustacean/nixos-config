a65c9a59c4af9ba75a786067bac6db3516692f02
Dilshad <a-rustacean@outlook.com>
2026-06-28 10:40:48 +0530

    chore: add hardware config

--- modules/hosts/nixos/hardware.nix
+++ modules/hosts/nixos/hardware.nix
@@ -1,4 +1,48 @@
 { ... }:
 {
-  flake.nixosModules.hardwareConfig = throw "Paste your hardware config here!";
+  flake.nixosModules.hardwareConfig =
+    {
+      config,
+      lib,
+      pkgs,
+      modulesPath,
+      ...
+    }:
+
+    {
+      imports = [
+        (modulesPath + "/profiles/qemu-guest.nix")
+      ];
+
+      boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "virtio_pci"
+        "usbhid"
+        "usb_storage"
+        "sr_mod"
+      ];
+      boot.initrd.kernelModules = [ ];
+      boot.kernelModules = [ ];
+      boot.extraModulePackages = [ ];
+
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/80b54540-1ef9-4928-8673-542a00c57ee9";
+        fsType = "ext4";
+      };
+
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/A5D0-F8F6";
+        fsType = "vfat";
+        options = [
+          "fmask=0077"
+          "dmask=0077"
+        ];
+      };
+
+      swapDevices = [
+        { device = "/dev/disk/by-uuid/49fbce46-ba1d-4458-aeef-15de90dd0b8c"; }
+      ];
+
+      nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+    };
 }
