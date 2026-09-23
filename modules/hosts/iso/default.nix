a9ca766a05a55c9eb51ed7a4a22c6a4854358b41
Dilshad <a-rustacean@outlook.com>
2026-09-23 17:42:13 +0530

    iso: add development modules, drop redundant comments

--- modules/hosts/iso/default.nix
+++ modules/hosts/iso/default.nix
@@ -7,9 +7,6 @@
       ...
     }:
     {
-      # Builds a live ISO of this repo's Hyprland desktop for the current
-      # system. From an aarch64 machine: `nix build .#iso` (native); for
-      # another arch, `nix build .#packages.<system>.iso`.
       packages.iso = self.lib.platformGuard {
         inherit pkgs;
         name = "iso";
@@ -18,17 +15,10 @@
             iso = inputs.nixpkgs.lib.nixosSystem {
               inherit system;
               modules = [
-                # Provides config.system.build.isoImage (a live bootable CD,
-                # not the installer). Imported by path because the cd-dvd
-                # installer modules are not part of nixpkgs' `nixosModules`
-                # set on this channel.
                 (inputs.nixpkgs.outPath + "/nixos/modules/installer/cd-dvd/iso-image.nix")
 
-                # The full Hyprland desktop from this repo. The ISO-specific
-                # filesystem and boot overrides (mkImageMediaOverride,
-                # priority 60) win over the plain values set here, just like
-                # on-disk installs.
                 self.nixosModules.desktop
+                self.nixosModules.development
 
                 {
                   # Boot as EFI and from a USB stick, in addition to BIOS.
