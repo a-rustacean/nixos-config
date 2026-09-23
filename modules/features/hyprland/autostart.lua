1f876e3843cbef476dc8658a324fd41359880dd2
Dilshad <a-rustacean@outlook.com>
2026-09-23 09:44:55 +0530

    hyprland: remove hyprlock on boot

--- modules/features/hyprland/autostart.lua
+++ modules/features/hyprland/autostart.lua
@@ -5,7 +5,6 @@
 local config = require("config")
 
 hl.on("hyprland.start", function()
-  hl.exec_cmd(config.programs.hyprlock)
   hl.exec_cmd(config.programs.hyprpaper)
   hl.exec_cmd(config.programs.quickshell)
   hl.exec_cmd(config.programs.dunst)
